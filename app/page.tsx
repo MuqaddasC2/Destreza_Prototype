1090620183415031a22f5c0a0f2629cfef99a10b
Ahmad Shahmeer <ahmadshahmeer26@gmail.com>
2025-05-15 21:31:12 +0500

    Fixed local running error

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>This is a placeholder for the app/page.tsx file.</h1>
+      <p>Replace this with your desired content.</p>
+    </div>
+  )
+}
+
+export default Page
